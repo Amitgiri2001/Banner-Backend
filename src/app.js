04a137b0d6cbd4b50388c9910efa39ce3ae4c5f1
Amit giri <92848403+Amitgiri2001@users.noreply.github.com>
2024-08-12 23:33:58 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,14 @@
 import express from 'express';
 import bannerRoutes from '../routes/bannerRoutes.js';
 
+const cors = require('cors');
 const app = express();
+
+app.use(cors({
+    origin: 'http://your-frontend-domain.com', // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+    credentials: true // If you need to send cookies or authentication headers
+}));
 app.use(express.json());
 
 // Routes
