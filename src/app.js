e600c31087b773642c375d3fb591c8b346f38230
Amit giri <92848403+Amitgiri2001@users.noreply.github.com>
2024-08-13 01:22:26 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,8 +6,7 @@ import cors from 'cors';
 const app = express();
 
 // app.use(cors());
-app.use(cors({ origin: 'banner-fronend-m4e8-git-master-amitgiri2001s-projects.vercel.app
-' }));
+app.use(cors({ origin: 'http://ec2-43-205-255-15.ap-south-1.compute.amazonaws.com/' }));
 app.use(express.json());
 
 // Routes
