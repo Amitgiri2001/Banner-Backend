dcb5fa62fdf14979c5f0a7aed9f65cfce2c810f2
Amit giri <92848403+Amitgiri2001@users.noreply.github.com>
2024-08-13 01:25:54 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 const app = express();
 
 // app.use(cors());
-app.use(cors({ origin: 'http://ec2-43-205-255-15.ap-south-1.compute.amazonaws.com/' }));
+app.use(cors({ origin: 'http://ec2-43-205-255-15.ap-south-1.compute.amazonaws.com' }));
 app.use(express.json());
 
 // Routes
