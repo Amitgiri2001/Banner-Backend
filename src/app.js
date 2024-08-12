91aff7e9c8fb14fa718e6eb794b1b71f4675acce
Amit giri <92848403+Amitgiri2001@users.noreply.github.com>
2024-08-13 00:03:02 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: 'http://your-frontend-domain.com', // Replace with your frontend URL
+    origin: 'https://banner-fronend-m4e8.vercel.app/', // Replace with your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
     credentials: true // If you need to send cookies or authentication headers
 }));
